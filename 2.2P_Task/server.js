b1e777a1dd4ec7c0defffbd0e1d9ecd9a36f42e1
Dhanush280 <73783265+DhanushChinivar@users.noreply.github.com>
2025-03-13 16:09:55 +1100

    2.2P Task

--- 2.2P_Task/server.js
+++ 2.2P_Task/server.js
@@ -1,23 +1,25 @@
-var express = require("express")
+var express = require("express");
 const path = require('path'); // Make sure to include this
-var app = express()
+var app = express();
 var port = process.env.port || 3003;
 
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// GET endpoint to calculate the square of a number
-// Example: http://localhost:3000/square?num=5
-app.get('/square', (req, res) => {
-  const num = parseFloat(req.query.num);
+// GET endpoint to calculate the sum of two numbers
+// Example: http://localhost:3003/add?a=5&b=3
+app.get('/add', (req, res) => {
+  const a = parseFloat(req.query.a);
+  const b = parseFloat(req.query.b);
   
-  if (isNaN(num)) {
-    return res.send("Error: Please provide a valid number using query parameter 'num'.");
+  if (isNaN(a) || isNaN(b)) {
+    return res.send("Error: Please provide valid numbers using query parameters 'a' and 'b'.");
   }
   
-  const square = num * num;
-  res.send(`The square of ${num} is: ${square}`);
+  const sum = a + b;
+  res.send(`The sum of ${a} and ${b} is: ${sum}`);
 });
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
